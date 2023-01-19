5f13b2de8bc7c59de453b8665da7391d49223fbd
Calapzu <yamsoncalapzu@gmail.com>
2023-01-18 21:09:14 -0500

    Se construyo la API REST de inicio de sesion/registro

--- src/main/java/com/sistema/blog/controlador/AuthControlador.java
+++ src/main/java/com/sistema/blog/controlador/AuthControlador.java
@@ -1,6 +1,11 @@
 package com.sistema.blog.controlador;
 
 import com.sistema.blog.dto.LoginDTO;
+import com.sistema.blog.dto.RegistroDTO;
+import com.sistema.blog.entidades.Rol;
+import com.sistema.blog.entidades.Usuario;
+import com.sistema.blog.repositorio.RolRepositorio;
+import com.sistema.blog.repositorio.UsuarioRespositorio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,10 +13,13 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -20,11 +28,42 @@ public class AuthControlador {
     @Autowired
     private AuthenticationManager authenticationManager;
 
+    @Autowired
+    private UsuarioRespositorio usuarioRespositorio;
+
+    @Autowired
+    private RolRepositorio rolRepositorio;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     @PostMapping("/iniciarSesion")
     public ResponseEntity<String> authenticateUser(@RequestBody LoginDTO loginDTO){
         Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginDTO.getUsernameOrEmail(), loginDTO.getPassword()));
 
         SecurityContextHolder.getContext().setAuthentication(authentication);
-        return new ResponseEntity<>("Ha iniciado sesion con exit!!", HttpStatus.OK );
+        return new ResponseEntity<>("Ha iniciado sesion con exito !!", HttpStatus.OK );
+    }
+
+    @PostMapping("/registrar")
+    public ResponseEntity<?> registrarUsusario(@RequestBody RegistroDTO registroDTO){
+        if (usuarioRespositorio.existsByUsername(registroDTO.getUsername())){
+            return new ResponseEntity<>("Ese nombre de usuario ya existe", HttpStatus.BAD_REQUEST);
+        }
+        if (usuarioRespositorio.existsByEmail(registroDTO.getEmail())){
+            return new ResponseEntity<>("Ese email de usuario ya existe", HttpStatus.BAD_REQUEST);
+        }
+
+        Usuario usuario = new Usuario();
+        usuario.setNombre(registroDTO.getNombre());
+        usuario.setUsername(registroDTO.getUsername());
+        usuario.setEmail(registroDTO.getEmail());
+        usuario.setPassword(passwordEncoder.encode(registroDTO.getPassword()));
+
+        Rol roles = rolRepositorio.findByNombre("ROLE_ADMIN").get();
+        usuario.setRoles(Collections.singleton(roles));
+
+        usuarioRespositorio.save(usuario);
+        return new ResponseEntity<>("Usuario registrar exitosamente", HttpStatus.OK);
     }
 }
--- src/main/java/com/sistema/blog/repositorio/RolRepositorio.java
+++ src/main/java/com/sistema/blog/repositorio/RolRepositorio.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface RolRepositorio extends JpaRepository<Usuario, Long> {
+public interface RolRepositorio extends JpaRepository<Rol, Long> {
 
     public Optional<Rol> findByNombre(String nombre);
 }
